d60b4c03bf3b2a1110b5c69bf90a5714d89276d0
Ariel Nuñez <ingenieroariel@gmail.com>
2016-07-08 11:53:28 -0500

    Added registry and maploom

--- piensanode/urls.py
+++ piensanode/urls.py
@@ -2,9 +2,18 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 from geonode.urls import *
+from maploom.geonode.urls import urlpatterns as maploom_urls
+from hypermap.urls import urlpatterns as registry_urls
 
 urlpatterns = patterns('',
    url(r'^/?$',
        TemplateView.as_view(template_name='site_index.html'),
        name='home'),
  ) + urlpatterns
+
+urlpatterns += maploom_urls
+urlpatterns += registry_urls
+
+urlpatterns = patterns('',
+    url(r'^registry/client/$', 'maploom_registry.views.lennin', name='maploom_registry'),
+) + urlpatterns
